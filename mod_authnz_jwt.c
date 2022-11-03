c5865aab3f3c11c054ee112c81c72959c6606637
Suvarna <suvarna.kaanuboyina@gds.ey.com>
2022-11-03 11:46:40 +0530

    Removed the unnecessary conditional check

--- mod_authnz_jwt.c
+++ mod_authnz_jwt.c
@@ -1090,14 +1090,7 @@ static int auth_jwt_login_handler(request_rec *r){
  	apr_size_t size;
 
  	int rv;
-	char *url_need_to_skip = "/gettoken";
-	if(strstr(r->uri, url_need_to_skip) == NULL){
-		ap_log_rerror(APLOG_MARK, APLOG_DEBUG, 0, r, APLOGNO(55200)
-							"auth_jwt authn: url: %s", r->uri);
-
-		return OK;
-	}
-		
+	
 
 	if(r->method_number != M_POST){
 		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(55201)
